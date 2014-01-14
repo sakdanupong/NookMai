fc37d50da14880c53cae2b20a9aade9ec3ef3d39
HLP MINI <chat@HLPs-Mac-mini.local>
2014-01-14 18:04:15 +0700

    merge

--- models/commentmodel.py
+++ models/commentmodel.py
@@ -1,9 +1,15 @@
+# from google.appengine.ext import ndb
+
+# class CommentModel(ndb.Model):
+#     movie_id = ndb.IntegerProperty()
+#     author = ndb.StringProperty(indexed=False)
+#     content = ndb.StringProperty(indexed=False)
+#     date = ndb.DateTimeProperty(auto_now_add=True)
+    
 from google.appengine.ext import db
 
 class CommentModel(db.Model):
-    id_movie = db.IntegerProperty()
-    id = db.IntegerProperty()
-    author = db.UserProperty()
+    movie_id = db.IntegerProperty()
+    author = db.StringProperty(indexed=False)
     content = db.StringProperty(indexed=False)
     date = db.DateTimeProperty(auto_now_add=True)
-    
